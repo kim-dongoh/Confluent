0ec4d4bfe8f838ec0e181da5b5f53bd6b52f9176
kim-dongoh <dongoh31277@gmail.com>
2022-10-04 02:49:06 +0000

    update docker-compose.yml

--- docker/multi_node/delete.sh
+++ docker/multi_node/delete.sh
@@ -8,13 +8,12 @@ docker-compose down --volumes
 docker rm -f $(docker ps -aq)
 docker rmi -f $(docker images)
 
-
-(cd "${DIR}/kafkaDir/data/" && rm -rf *)
+(cd "${DIR}/kafkaDir/kafka1/data/" && rm -rf *)
+(cd "${DIR}/kafkaDir/kafka2/data/" && rm -rf *)
+(cd "${DIR}/kafkaDir/kafka3/data/" && rm -rf *)
 (cd "${DIR}/zookeeperDir/zookeeper1/data/" && rm -rf *)
 (cd "${DIR}/zookeeperDir/zookeeper1/log/" && rm -rf *)
 (cd "${DIR}/zookeeperDir/zookeeper2/data/" && rm -rf *)
 (cd "${DIR}/zookeeperDir/zookeeper2/log/" && rm -rf *)
 (cd "${DIR}/zookeeperDir/zookeeper3/data/" && rm -rf *)
 (cd "${DIR}/zookeeperDir/zookeeper3/log/" && rm -rf *)
-#(cd "${DIR}/zookeeperDir/data/" && rm -rf *)
-#(cd "${DIR}/zookeeperDir/log/" && rm -rf *)
